3babe9b50594218d8074039e601c2f410e0f537f
Erik Marques <santanaerik1106@gmail.com>
2021-11-18 18:39:10 -0300

    Create carros-update.php

---
+++ Views/Carros/carros-update.php
@@ -0,0 +1,40 @@
+<!-- Header redireciona para o index -->
+<!-- header('location: carros-index.php'); -->
+<?php
+// Verifica se todos os campos estão preenchidos
+header('location: carros-index.php');
+spl_autoload_register(function ($class_name) {
+  include '..\\..\\' . $class_name . '.php';
+});
+
+use Models\Cliente;
+use Models\Veiculo;
+use Models\Endereco;
+use Models\Pagamento;
+use Db\Persiste;
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+if (
+  isset($_POST['nome']) &&
+  isset($_POST['cpf']) &&
+  isset($_POST['telefone']) &&
+  isset($_POST['logradouro']) &&
+  isset($_POST['numero']) &&
+  isset($_POST['cep']) &&
+  isset($_POST['bairro']) &&
+  isset($_POST['cidade']) &&
+  isset($_POST['estado']) &&
+  isset($_POST['marca']) &&
+  isset($_POST['modelo']) &&
+  isset($_POST['ano']) &&
+  isset($_POST['preco']) &&
+  isset($_POST['placa']) &&
+  isset($_GET["id"])
+) {
+  $novoCliente = new Cliente($_GET["id"], $_POST['nome'], $_POST['cpf'], $_POST['telefone']);
+  $novoVeiculo = new Veiculo($_GET["id"], $_POST['marca'], $_POST['modelo'], $_POST['ano'], $_POST['placa'], $_POST['preco']);
+  $novoEndereco = new Endereco($_GET["id"], $_GET["id"], $_POST['logradouro'], $_POST['numero'], $_POST['bairro'], $_POST['cidade'], $_POST['estado'], $_POST['cep']);
+  $p = Persiste::GetById('Models\Pagamento', $_GET['id']);
+  $novoPagamento = new Pagamento($_GET["id"], $_GET["id"], $_GET["id"], $_POST['preco'], $p->getdata_pagamento);
+}
