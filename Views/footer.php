0de7cfea955fbb4c25a792272a9195d816e7ecda
Erik Marques <santanaerik1106@gmail.com>
2021-11-18 11:44:51 -0300

    Create footer.php

---
+++ Views/footer.php
@@ -0,0 +1,17 @@
+  <footer class="ui inverted vertical footer segment form-page">
+    <div class="container-footer" style="max-width: fit-content; margin: 0 auto;">
+      <p>Criado por </p>
+      <p><b>Davidson di David</b>,</p>
+      <p><b>Erik Santana</b>,</p>
+      <p><b>Jefferson Leonardo</b>,</p>
+      <p><b>Maithê de Souza</b></p>
+    </div>
+    <div style="max-width: fit-content; margin: 0 auto;">
+      <a style="max-width: fit-content; margin: 0 auto;" href="">Link do repositório</a>
+    </div>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+  </body>
+
+  </html>
