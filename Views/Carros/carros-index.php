64d92abeffe127a6cfa1bbb748cff42f5c8fda63
Erik Marques <santanaerik1106@gmail.com>
2021-11-18 11:44:56 -0300

    Create carros-index.php

---
+++ Views/Carros/carros-index.php
@@ -0,0 +1,22 @@
+<?php include '..\header.php'; ?>
+
+<!-- Estilização única -->
+<style>
+
+</style>
+
+<main>
+  <h1>Tabela de carros alugados</h1>
+  <div style="width: 100%;" class="ui divider"></div>
+  <a href="/Views/Carros/carros-create.php">
+    <button class="ui right floated button">Novo veículo</button>
+  </a>
+
+</main>
+
+<!-- Muda o titulo da página -->
+<script>
+  document.title = "Tabela de carros"
+</script>
+
+<?php include '..\footer.php'; ?>
