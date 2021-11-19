8df4fd52d54342a24df583952929d9f5fb71090b
Erik Marques <santanaerik1106@gmail.com>
2021-11-19 19:04:17 -0300

    Create Home.tsx

---
+++ AplicaçãoReact/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { Header } from "semantic-ui-react";
+
+const Home: FC = () => {
+  return (
+    <main>
+      <Header as="h1">Página inicial</Header>
+      <Header as="h2">TP de PHP - Locação de veículos</Header>
+    </main>
+  );
+};
+
+export default Home;
