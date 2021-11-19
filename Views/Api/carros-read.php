c1487df292b29d612cb3a2d9f3fd3b7f11a063bc
Erik Marques <santanaerik1106@gmail.com>
2021-11-19 12:04:40 -0300

    fix: get id

--- Views/Api/carros-read.php
+++ Views/Api/carros-read.php
@@ -7,7 +7,45 @@ use Db\Persiste;
 
 $result = [];
 
-if (isset($_GET['id'])) {
+if (isset($_GET['id']) && isset($_GET['page']) && isset($_GET['limit'])) {
+  $clientes = Persiste::GetPaginate('Models\Cliente', isset($_GET['page']), isset($_GET['limit']));
+  $veiculos = Persiste::GetPaginate('Models\Veiculo', isset($_GET['page']), isset($_GET['limit']));
+  $pagamentos = Persiste::GetPaginate('Models\Pagamento', isset($_GET['page']), isset($_GET['limit']));
+  $enderecos = Persiste::GetPaginate('Models\Endereco', isset($_GET['page']), isset($_GET['limit']));
+  if (is_array($clientes) && is_array($veiculos) && is_array($pagamentos) && is_array($enderecos)) {
+    for ($i = 0; $i < count($clientes); ++$i) {
+      $v = $veiculos[$i];
+      $c = $clientes[$i];
+      $p = $pagamentos[$i];
+      $e = $enderecos[$i];
+      $vet = [
+        'id' => $c->getid,
+        'nome' => $c->getnome,
+        'cpf' => $c->getcpf,
+        'telefone' => $c->gettelefone,
+        'logradouro' => $e->getlogradouro,
+        'numero' => $e->getnumero,
+        'bairro' => $e->getbairro,
+        'cidade' => $e->getcidade,
+        'estado' => $e->getestado,
+        'cep' => $e->getcep,
+        'marca' => $v->getmarca,
+        'modelo' => $v->getmodelo,
+        'preco' => $v->getpreco,
+        'placa' => $v->getplaca,
+        'ano' => $v->getano,
+        'data_pagamento' => $p->getdata_pagamento,
+      ];
+      $result[$i] = $vet;
+    }
+    http_response_code(200);
+    echo json_encode($result);
+  } else {
+    http_response_code(500);
+    $result = ['error' => "Internal Server Error."];
+    echo json_encode($result);
+  }
+} else if (isset($_GET['id'])) {
   $c = Persiste::GetById('Models\Cliente', $_GET['id']);
   $v = Persiste::GetById('Models\Veiculo', $_GET['id']);
   $e = Persiste::GetById('Models\Endereco', $_GET['id']);
@@ -34,112 +72,46 @@ if (isset($_GET['id'])) {
     http_response_code(200);
     echo json_encode($result);
   } else {
-    $result = ['error' => "User not found."];
     http_response_code(404);
+    $result = ['error' => "User not found."];
     echo json_encode($result);
   }
 } else {
-  if (isset($_GET['page']) || isset($_GET['limit'])) {
-    if (isset($_GET['page'])) {
-      $page = $_GET['page'];
-    } else {
-      $page = 1;
+  $clientes = Persiste::GetAll('Models\Cliente');
+  $veiculos = Persiste::GetAll('Models\Veiculo');
+  $pagamentos = Persiste::GetAll('Models\Pagamento');
+  $enderecos = Persiste::GetAll('Models\Endereco');
+  if (is_array($clientes) && is_array($veiculos) && is_array($pagamentos) && is_array($enderecos)) {
+    for ($i = 0; $i < count($clientes); ++$i) {
+      $v = $veiculos[$i];
+      $c = $clientes[$i];
+      $p = $pagamentos[$i];
+      $e = $enderecos[$i];
+      $vet = [
+        'id' => $c->getid,
+        'nome' => $c->getnome,
+        'cpf' => $c->getcpf,
+        'telefone' => $c->gettelefone,
+        'logradouro' => $e->getlogradouro,
+        'numero' => $e->getnumero,
+        'bairro' => $e->getbairro,
+        'cidade' => $e->getcidade,
+        'estado' => $e->getestado,
+        'cep' => $e->getcep,
+        'marca' => $v->getmarca,
+        'modelo' => $v->getmodelo,
+        'preco' => $v->getpreco,
+        'placa' => $v->getplaca,
+        'ano' => $v->getano,
+        'data_pagamento' => $p->getdata_pagamento,
+      ];
+      $result[$i] = $vet;
     }
-
-    if (isset($_GET['limit'])) {
-      $limit = $_GET['limit'];
-    } else {
-      $limit = 10;
-    }
-
-    $inicio = $page - 1;
-    $inicio = $inicio * $limit;
-    $clientes = Persiste::GetPaginate('Models\Cliente', $inicio, $limit);
-    $veiculos = Persiste::GetPaginate('Models\Veiculo', $inicio, $limit);
-    $pagamentos = Persiste::GetPaginate('Models\Pagamento', $inicio, $limit);
-    $enderecos = Persiste::GetPaginate('Models\Endereco', $inicio, $limit);
-    if (is_array($clientes)) {
-      for ($i = 0; $i < count($clientes); ++$i) {
-        $v = $veiculos[$i];
-        $c = $clientes[$i];
-        $p = $pagamentos[$i];
-        $e = $enderecos[$i];
-        $vet = [
-          'id' => $c->getid,
-          'nome' => $c->getnome,
-          'cpf' => $c->getcpf,
-          'telefone' => $c->gettelefone,
-          'logradouro' => $e->getlogradouro,
-          'numero' => $e->getnumero,
-          'bairro' => $e->getbairro,
-          'cidade' => $e->getcidade,
-          'estado' => $e->getestado,
-          'cep' => $e->getcep,
-          'marca' => $v->getmarca,
-          'modelo' => $v->getmodelo,
-          'preco' => $v->getpreco,
-          'placa' => $v->getplaca,
-          'ano' => $v->getano,
-          'data_pagamento' => $p->getdata_pagamento,
-        ];
-        $result[$i] = $vet;
-      }
-      if (count($clientes) == 0) {
-        http_response_code(404);
-        $result = ['error' => "Page not found."];
-        echo json_encode($result);
-      } else {
-        http_response_code(200);
-        echo json_encode($result);
-      }
-    } else {
-      http_response_code(404);
-      $result = ['error' => "Users not found."];
-      echo json_encode($result);
-    }
+    http_response_code(200);
+    echo json_encode($result);
   } else {
-    $clientes = Persiste::GetAll('Models\Cliente');
-    $veiculos = Persiste::GetAll('Models\Veiculo');
-    $pagamentos = Persiste::GetAll('Models\Pagamento');
-    $enderecos = Persiste::GetAll('Models\Endereco');
-    if (is_array($clientes)) {
-      for ($i = 0; $i < count($clientes); ++$i) {
-        $v = $veiculos[$i];
-        $c = $clientes[$i];
-        $p = $pagamentos[$i];
-        $e = $enderecos[$i];
-        $vet = [
-          'id' => $c->getid,
-          'nome' => $c->getnome,
-          'cpf' => $c->getcpf,
-          'telefone' => $c->gettelefone,
-          'logradouro' => $e->getlogradouro,
-          'numero' => $e->getnumero,
-          'bairro' => $e->getbairro,
-          'cidade' => $e->getcidade,
-          'estado' => $e->getestado,
-          'cep' => $e->getcep,
-          'marca' => $v->getmarca,
-          'modelo' => $v->getmodelo,
-          'preco' => $v->getpreco,
-          'placa' => $v->getplaca,
-          'ano' => $v->getano,
-          'data_pagamento' => $p->getdata_pagamento,
-        ];
-        $result[$i] = $vet;
-      }
-      if (count($clientes) == 0) {
-        http_response_code(404);
-        $result = ['error' => "Users not found."];
-        echo json_encode($result);
-      } else {
-        http_response_code(200);
-        echo json_encode($result);
-      }
-    } else {
-      http_response_code(500);
-      $result = ['error' => "Internal Server Error."];
-      echo json_encode($result);
-    }
+    http_response_code(500);
+    $result = ['error' => "Internal Server Error."];
+    echo json_encode($result);
   }
 }
