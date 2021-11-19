354398ec102017d7108ffc19f19b5870cacafcbc
Erik Marques <santanaerik1106@gmail.com>
2021-11-19 12:16:21 -0300

    create a new folder so I delete that

--- Db/Conexao.php
+++
@@ -1,4 +0,0 @@
-<?php
-const DB_USER = 'root';
-const DB_PASS = '1234';
-const DB = 'mysql:host=localhost;port=3306;dbname=locacao';
--- Models/IDados.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Models;
-
-interface IDados
-{
-  public function toJson();
-  public function toArray();
-}
